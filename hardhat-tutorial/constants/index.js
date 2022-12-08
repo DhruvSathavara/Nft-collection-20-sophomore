0a4d19ae3ced9e2e7467ba0e04dbd3ae0283f01e
DhruvSathavara <dhruvsathavara7228@gmail.com>
2022-12-08 09:36:45 +0530

    sc

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed in whitelist Dapp tutorial
 const WHITELIST_CONTRACT_ADDRESS = "0xdbedfDeddE4fD80960507dC4e573389213F3E455";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-20-sophomore.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
